2c767945af0bc91c5369ec3bb529eeb4171e9a9c
mmrosatab <mmrosatab@hotmail.com>
2022-06-08 22:15:52 -0300

    Feature: create controller negociacao

--- typescript-curso-1/app/models/negociacao.ts
+++ typescript-curso-1/app/models/negociacao.ts
@@ -1,23 +1,23 @@
 export class Negociacao {
-  private _data;
-  private _quantidade;
-  private _valor;
+  private _data: Date;
+  private _quantidade: number;
+  private _valor: number;
 
-  constructor(data, quantidade, valor) {
+  constructor(data: Date, quantidade: number, valor: number) {
     this._data = data;
     this._quantidade = quantidade;
     this._valor = valor;
   }
 
-  get data() {
+  get data(): Date {
     return this.data;
   }
 
-  get quantidade() {
+  get quantidade(): number {
     return this.quantidade;
   }
 
-  get volume() {
+  get volume(): number {
     return this._quantidade * this._valor;
   }
 }
