2f09c0dbe81c2c7063ea147ebe8f70e92f680cfd
mmrosatab <mmrosatab@hotmail.com>
2022-06-18 18:46:42 -0300

    Refactor: Handle null values on querySelector

--- typescript-curso-1/dist/js/models/negociacoes.js
+++ typescript-curso-1/dist/js/models/negociacoes.js
@@ -1,11 +1,7 @@
 export class Negociacoes {
     constructor() {
         this.negociacoes = [];
-        // lista(): readonly Negociacao[] {
-        //   return [...this.negociacoes];
-        // }
     }
-    // private negociacoes: Negociacao[] = []; same thing
     adiciona(negociacao) {
         this.negociacoes.push(negociacao);
     }
